4677ae05bb427332f8de860ee0b07875ba9ecd50
picassoeason <easonwu.student@outlook.com>
2024-09-07 19:29:11 +0800

    ChangIcon

--- src/components/ServiceButtons.js
+++ src/components/ServiceButtons.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Heart, Info } from 'lucide-react';
+import { Heart, Info,Cookie,BedSingle,HeartHandshake,Accessibility } from 'lucide-react';
+
 
 const ServiceButtons = () => (
   <div className="flex space-x-2 p-4">
-    <button className="flex-1 bg-blue-100 text-blue-600 py-2 rounded-full flex items-center justify-center">
-      <Info size={16} className="mr-2" />
-      社會救助
+    <button className="flex-1 bg-blue-100 text-blue-600 py-2 rounded-full flex items-center justify-center" >
+      <HeartHandshake size={16} className="mr-2" />
+      社會救助中心
     </button>
     <button className="flex-1 bg-red-100 text-red-600 py-2 rounded-full flex items-center justify-center">
-      <Heart size={16} className="mr-2" />
-      愛心餐
+      <Cookie size={16} className="mr-2" />
+      肚子餓了(愛心餐廳)
+    </button>
+    <button className="flex-1 bg-orange-200 text-orange-700 py-2 rounded-full flex items-center justify-center">
+      <BedSingle size={16} className="mr-3" />
+      臨時住宿
+    </button>
+    <button className="flex-1 bg-purple-400 text-white py-2 rounded-full flex items-center justify-center">
+      <Accessibility size={16} className="mr-2" />
+      需要支援
     </button>
   </div>
 );
